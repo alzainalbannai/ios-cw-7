e8cc1349ba8408f355e5b2f115b89fec545cd858
alzainalbannai <alzainalbannai@users.noreply.github.com>
2020-07-05 20:43:56 +0300

    done
    done

---
+++ IOSClasswork7/IOSClasswork7/Storyboard/masterViewVCViewController.swift
@@ -0,0 +1,30 @@
+//
+//  masterViewVCViewController.swift
+//  IOSClasswork7
+//
+//  Created by Alzain  on 7/4/20.
+//  Copyright © 2020 Safeyah Coding. All rights reserved.
+//
+
+import UIKit
+
+class masterViewVCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
